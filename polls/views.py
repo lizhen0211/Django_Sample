bc9a4f998a23c8f1e8a07dac8057e123641fd891
lizhen <lizhen0211@163.com>
2018-10-26 11:39:59 +0800

    使用模板系统

--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,41 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
 
 from polls.models import Question
 
 
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
+
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+
+    # 一个快捷函数： render()¶「载入模板，填充上下文，再返回由它生成的 HttpResponse对象」是一个非常常用的操作流程。
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output + " %s." % question_id)
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output + " %s." % question_id)
+
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, 'polls/detail.html', {'question': question})
+
+    # 一个快捷函数： get_object_or_404() 尝试用get() 函数获取一个对象，如果不存在就抛出 Http404 错误也是一个普遍的流程。
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
